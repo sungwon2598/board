d16e37cc1a0c36d4ef9596c5a216abf54bdc1fcf
sungwon2598 <tjddnjs2598@gmail.com>
2024-01-16 13:53:04 +0900

    feat(Board) : 민원처리상태 추가(view)

--- src/main/java/ict/board/domain/board/BoardStatus.java
+++ src/main/java/ict/board/domain/board/BoardStatus.java
@@ -1,7 +1,17 @@
 package ict.board.domain.board;
 
 public enum BoardStatus {
-    UNCHECKED, // 미확인
-    IN_PROGRESS, // 처리중
-    COMPLETED // 처리완료
+    UNCHECKED("미확인"), // 미확인
+    IN_PROGRESS("처리중"), // 처리중
+    COMPLETED("처리완료"); // 처리완료
+
+    private final String koreanName;
+
+    BoardStatus(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
