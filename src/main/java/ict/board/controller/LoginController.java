d59a5573663f4afae2a740d5e1d7af6c568a6af6
sungwon2598 <tjddnjs2598@gmail.com>
2024-05-30 09:30:08 +0900

    feat(login) : 로그인 양식 및 비밀번호 및 아이디 매칭 안될시 사용자에게 오류 메시지 피드백 추가

--- src/main/java/ict/board/controller/LoginController.java
+++ src/main/java/ict/board/controller/LoginController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,25 +29,29 @@ public class LoginController {
 
     private final LoginService loginService;
 
-    @GetMapping("login")
-    public String login(Model model) {
-        model.addAttribute("LoginForm", new LoginForm());
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
 
-    @PostMapping("login")
-    public String myPage(@Valid LoginForm form, BindingResult result, HttpServletRequest request,
-                         @RequestParam(defaultValue = "/") String redirectURL) {
+    @PostMapping("/login")
+    public String myPage(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult result, HttpServletRequest request,
+                         @RequestParam(defaultValue = "/") String redirectURL, Model model) {
 
         if (result.hasErrors()) {
             return "login";
         }
 
-        String loginMemberEmail = loginService.login(form.getEmail(), form.getPassword()).getEmail();
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMemberEmail);
-
-        return "redirect:" + redirectURL;
+        try {
+            String loginMemberEmail = loginService.login(form.getEmail(), form.getPassword()).getEmail();
+            HttpSession session = request.getSession();
+            session.setAttribute(SessionConst.LOGIN_MEMBER, loginMemberEmail);
+            return "redirect:" + redirectURL;
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("loginError", e.getMessage());
+            return "login";
+        }
     }
 
     @PostMapping("/logout")
--- src/main/java/ict/board/service/LoginService.java
+++ src/main/java/ict/board/service/LoginService.java
@@ -13,16 +13,14 @@ public class LoginService {
     private final MemberRepository memberRepository;
 
     public Member login(String email, String password) {
-
         Member member = memberRepository.findMemberByEmail(email).orElseThrow(() ->
-                new IllegalStateException("회원이 존재하지 않습니다.")
+                new IllegalArgumentException("회원이 존재하지 않습니다.")
         );
 
         if (!BCrypt.checkpw(password, member.getPassword())) {
-            throw new IllegalStateException("비밀번호를 다시 입력해주세요.");
+            throw new IllegalArgumentException("비밀번호를 다시 입력해주세요.");
         }
         return member;
     }
 
 }
-
