1be0ebd4b399629983c20112ef368c6700851a36
sungwon2598 <tjddnjs2598@gmail.com>
2024-06-24 21:34:14 +0900

    refactor(Interceptor) : 로그인 확인 및 Url별 권한페이지 접근 인터셉트 통합

---
+++ src/main/java/ict/board/interceptor/CombinedInterceptor.java
@@ -0,0 +1,53 @@
+package ict.board.interceptor;
+
+import ict.board.constant.SessionConst;
+import ict.board.domain.member.IctStaffMember;
+import ict.board.domain.member.Member;
+import ict.board.domain.member.Role;
+import ict.board.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CombinedInterceptor implements HandlerInterceptor {
+
+    private final MemberService memberService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        String email = (String) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        Member member = memberService.findMemberByEmail(email);
+        String uri = request.getRequestURI();
+
+        if (uri.startsWith("/admin") || uri.startsWith("/staff-only") || uri.startsWith("/manage")) {
+            if (member instanceof IctStaffMember staffMember) {
+                if (staffMember.getRole() == Role.ADMIN && (uri.startsWith("/admin") || uri.startsWith("/manage"))) {
+                    return true;
+                } else if (staffMember.getRole() == Role.MANAGER && uri.startsWith("/manage")) {
+                    return true;
+                } else {
+                    response.sendRedirect("/access-denied-not-manage");
+                    return false;
+                }
+            }
+            response.sendRedirect("/access-denied");
+            return false;
+        }
+
+        return true;
+    }
+}
