265d9df075ed0c3a9a6b0cddff6b74de65378062
sungwon2598 <tjddnjs2598@gmail.com>
2024-09-13 12:27:17 +0900

    refactor(AsyncConfig) : 메모리 최적화 테스트를 위해 async설정 기본값으로 변경

--- src/main/java/ict/board/config/AsyncConfig.java
+++ src/main/java/ict/board/config/AsyncConfig.java
@@ -10,14 +10,14 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig extends AsyncConfigurerSupport {
 
-    @Override
-    public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(25);
-        executor.setThreadNamePrefix("AsyncExecutor-");
-        executor.initialize();
-        return executor;
-    }
+//    @Override
+//    public Executor getAsyncExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(5);
+//        executor.setMaxPoolSize(10);
+//        executor.setQueueCapacity(25);
+//        executor.setThreadNamePrefix("AsyncExecutor-");
+//        executor.initialize();
+//        return executor;
+//    }
 }
